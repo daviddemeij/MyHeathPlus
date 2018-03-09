926bdb8e975937a80f442488eeb8dc7ae375b4c9
David de Meij <daviddemeij@gmail.com>
2018-03-09 20:54:37 +0100

    Add nutrition value to food records and implement export to csv in admin

--- food_input/admin.py
+++ food_input/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .forms import FoodRecordForm
 # Register your models here.
 from .models import Product, FoodRecord
+from django.http import HttpResponse, HttpResponseForbidden
+from .actions import export_as_csv_action
+
 
 class FoodRecordAdmin(admin.ModelAdmin):
     list_display = ('patient_id', 'datetime', 'amount', 'product')
+    fields = [field.name for field in FoodRecord._meta.fields + FoodRecord._meta.many_to_many]
+    actions = [export_as_csv_action("CSV Export", fields=fields)]
 
-
-admin.site.register(FoodRecord)
+admin.site.register(FoodRecord, FoodRecordAdmin)
+admin.site.register(Product)
