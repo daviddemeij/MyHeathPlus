443bf1fba74135551328992635aa63b2add7c4d8
Daviddemeij <daviddemeij@gmail.com>
2018-03-20 14:57:23 +0000

    make product search insensitive to capitalization

--- food_input/views.py
+++ food_input/views.py
@@ -61,7 +61,7 @@ class ProductAutocomplete(autocomplete.Select2QuerySetView):
             qs = qs.filter(productgroep_oms=category)
 
         if self.q:
-            qs = qs.filter(product_omschrijving__contains=self.q)
+            qs = qs.filter(product_omschrijving__icontains=self.q)
 
         return qs
 
