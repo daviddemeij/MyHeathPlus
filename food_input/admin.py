8a3a9b75b74c4a356d1561b786f3b83b92020d45
David de Meij <daviddemeij@gmail.com>
2018-03-25 13:50:01 +0200

    remove unused code in admin.py

--- food_input/admin.py
+++ food_input/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from .forms import FoodRecordForm
-# Register your models here.
 from .models import Product, FoodRecord, Measurement
-from django.http import HttpResponse, HttpResponseForbidden
 from .actions import export_as_csv_action
 from .forms import MeasurementForm
-from django.contrib.auth.models import User
 
 @admin.register(FoodRecord)
 class FoodRecordAdmin(admin.ModelAdmin):
@@ -14,16 +10,8 @@ class FoodRecordAdmin(admin.ModelAdmin):
     actions = [export_as_csv_action("CSV Export", fields=product_fields)]
     list_filter = ('creator', 'patient_id')
 
-
-
-#class FoodRecordAdmin(admin.ModelAdmin):
-#    list_display = ('patient_id', 'datetime', 'amount', 'product')
-#    product_fields = [str(field.name) for field in FoodRecord._meta.get_fields()][1:]
-#    actions = [export_as_csv_action("CSV Export", fields=product_fields)]
-
 class MeasurementAdmin(admin.ModelAdmin):
     form = MeasurementForm
 
-#admin.site.register(FoodRecord, FoodRecordAdmin)
 admin.site.register(Product)
 admin.site.register(Measurement, MeasurementAdmin)
