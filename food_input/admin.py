0946a0298e4aa5c46f50e9fa91eba7b95724dc8e
David de Meij <daviddemeij@gmail.com>
2018-04-07 18:42:05 +0200

    update admin interface

--- food_input/admin.py
+++ food_input/admin.py
@@ -5,7 +5,7 @@ from .forms import MeasurementForm
 
 @admin.register(FoodRecord)
 class FoodRecordAdmin(admin.ModelAdmin):
-    list_display = ('patient_id', 'datetime', 'amount', 'product', 'creator')
+    list_display = ('id', 'patient_id', 'datetime', 'amount', 'product', 'creator')
     product_fields = [str(field.name) for field in FoodRecord._meta.get_fields()][1:]
     actions = [export_as_csv_action("CSV Export", fields=product_fields)]
     list_filter = ('creator', 'patient_id')
@@ -14,5 +14,10 @@ class MeasurementAdmin(admin.ModelAdmin):
     form = MeasurementForm
     list_display = ('name', 'amount')
     filter_vertical = ('linked_product',)
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_omschrijving', 'productgroep_oms', 'occurrence')
+    list_filter = ('productgroep_oms',)
+
 admin.site.register(Measurement, MeasurementAdmin)
