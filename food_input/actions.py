4a2ebb50d4df09e981761f4ac318529a440bb231
David de Meij <daviddemeij@gmail.com>
2018-04-08 15:12:54 +0200

    Add reset_display_names action

--- food_input/actions.py
+++ food_input/actions.py
@@ -1,6 +1,6 @@
 import unicodecsv
 from django.http import HttpResponse
-from .models import Product, FoodRecord
+from .models import Product, FoodRecord, DisplayName
 
 def export_as_csv_action(description="Export selected objects as CSV file",
                          fields=None, exclude=None, header=True):
@@ -42,3 +42,24 @@ def count_occurrence():
             product.save()
         occurrence_list.append((product.product_omschrijving, occurrence))
     return occurrence_list
+
+def reset_display_names():
+    for display_name in DisplayName.objects.all():
+        display_name.delete()
+
+    products = Product.objects.all()
+    for product in products:
+        name = product.product_omschrijving
+        if ("- " in name and "- en" not in name):
+            pos1 = name.find(" ")
+            pos2 = name.find("- ")
+            name2 = name[pos1+1].upper()+name[pos1+2:pos2]+name[:pos1].lower()+name[pos2+1:]
+            print(name, " => ", name2)
+            name = name2
+        elif name[-1] == "-":
+            pos1 = name.find(" ")
+            name2 = name[pos1+1].upper()+name[pos1+2:-1]+name[:pos1].lower()
+            print(name, " => ", name2)
+            name = name2
+
+        DisplayName.objects.create(product=product, name=name)
