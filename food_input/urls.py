9f87643cdbac450e04bcb24b74eb6e16013287d4
David de Meij <daviddemeij@gmail.com>
2018-04-14 16:43:15 +0200

    add urls api

--- food_input/urls.py
+++ food_input/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.urls import path
 from . import views
+from . import api
 from .actions import count_occurrence
+from rest_framework_jwt.views import obtain_jwt_token
 print("Counting occurrence!")
 count_occurrence()
 
@@ -13,4 +15,12 @@ urlpatterns = [path('', views.home),
                views.MeasurementAutocomplete.as_view(),
                name='measurement-autocomplete'),
                path('<int:id>/', views.delete_record, name="delete_record"),
-               path('count/', views.count)]
+               path('count/', views.count),
+               url('api/login/', obtain_jwt_token),
+               url('api/register/', api.register),
+               path('api/food_records/<int:id>/', api.food_record, name="food_record"),
+               url('api/food_records/', api.food_record_list),
+               url('api/measurements/', api.measurement_list),
+               url('api/products/', api.product_list)
+
+               ]
